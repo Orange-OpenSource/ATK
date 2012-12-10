af236c3b941b98987b7ef5fe9553b70575597191
pcrepieux <pcrepieux@gmail.com>
2012-12-10 10:28:28 +0100

    add a batch script to launch ATK with commandline

---
+++ install/ATK.bat
@@ -0,0 +1,31 @@
+
+@ECHO OFF
+
+setlocal ENABLEDELAYEDEXPANSION
+SET ATKParam=
+FOR /R jar %%G IN (*.jar) DO set CLASSPATH=!CLASSPATH!;%%G
+Echo The Classpath definition is %CLASSPATH%
+
+IF "%1" == "-gui" GOTO GUI
+:Loop
+
+	IF "%1"=="" GOTO NOGUI
+
+	SET ATKParam=%ATKParam% %1
+
+	SHIFT
+
+GOTO Loop
+
+:NOGUI
+
+REM if defined CLASSPATH (set CLASSPATH=%CLASSPATH%;.) else (set CLASSPATH=.)
+java com.orange.atk.launcher.LaunchJATK %ATKParam%
+GOTO END
+
+:GUI
+
+java com.orange.atk.launcher.LaunchGUIJATK
+
+:END
+Echo Thanks for using ATK !
