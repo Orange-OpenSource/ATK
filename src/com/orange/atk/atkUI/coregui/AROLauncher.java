ffff0b12d9e96d43c62a990bd280359af3a5d65e
Pierre Crépieux <pierre.crepieux@orange.com>
2014-06-27 15:00:56 +0200

    cleanup ARO launcher to work on mac

--- src/com/orange/atk/atkUI/coregui/AROLauncher.java
+++ src/com/orange/atk/atkUI/coregui/AROLauncher.java
@@ -24,6 +24,7 @@
 package com.orange.atk.atkUI.coregui;
 
 import com.orange.atk.atkUI.corecli.Configuration;
+import com.orange.atk.platform.Platform;
 import org.apache.log4j.Logger;
 
 import java.io.File;
@@ -31,12 +32,13 @@ import java.io.IOException;
 
 public class AROLauncher {
     private static final String AROPATH = Configuration.getProperty(Configuration.AROPATH);
-    private static final String LAUNCHARO = AROPATH + "\\bin\\aro.exe";
+    private static final String LAUNCHARO = AROPATH + Platform.FILE_SEPARATOR + "bin" +
+    Platform.FILE_SEPARATOR + "aro";
 
     private static ProcessBuilder initProcessBuilder(){
         Logger.getLogger(AROLauncher.class).info(LAUNCHARO + " from " + AROPATH);
         ProcessBuilder pb = new ProcessBuilder();
-        pb.directory(new File(AROPATH + "\\bin"));
+        pb.directory(new File(AROPATH + Platform.FILE_SEPARATOR + "bin"));
         pb.inheritIO();
         return pb;
     }
