3965ce54edaa63b152f3753a5dcc5113c1e3918d
Francois Jegou <francois.jegou@orange.com>
2014-08-27 15:23:47 +0200

    typo

--- src/com/orange/atk/results/measurement/MeasurementThread.java
+++ src/com/orange/atk/results/measurement/MeasurementThread.java
@@ -38,7 +38,7 @@ import com.orange.atk.results.logger.log.ResultLogger;
 
 
 /**
- * This class is the thread which periodically measure values from battery,
+ * This class is the thread which periodically measures values from battery,
  * memory, ..
  */
 
